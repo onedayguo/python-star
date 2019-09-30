58211464d4dc9be6ed73cae4cc25757914674f7d
kami <1402702693@qq.com>
2019-09-30 23:35:38 +0800

    leetcode78题，寻找单词。深度优先遍历dfs

--- leetcode/Leetcode1.py
+++ leetcode/Leetcode1.py
@@ -1,12 +1,15 @@
 # 开始使用python完成LeetCode上的算法题
+# --coding:utf-8
 
-# 78. Subsets
-# Given a set of distinct integers, nums, return all possible subsets (the power set).
-# Note: The solution set must not contain duplicate subsets.
 from typing import List
+
+from past.builtins import xrange
 
 
 class Solution:
+    # 78. Subsets
+    # Given a set of distinct integers, nums, return all possible subsets (the power set).
+    # Note: The solution set must not contain duplicate subsets.
     # def subsets(self, nums: List[int]) -> List[List[int]]:
     def subsets1(self, nums):
         res = []
@@ -24,11 +27,32 @@ class Solution:
             res += [item + [num] for item in res]
         return res
 
-    #def exist(self, board: List[List[str]], word: str) -> bool:
+    '''79.word search
+    Given a 2D board and a word, find if the word exists in the grid.
+    The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or
+    vertically neighboring. The same letter cell may not be used more than once.
+    '''
 
+    def exist(self, board, word):
+        if not board:
+            return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.dfs(board, i, j, word):
+                    return True
+        return False
 
+        # check whether can find word, start at (i,j) position
 
-if __name__ == "__main__":
-    Solution.subsets1()
-
-
+    def dfs(self, board, i, j, word):
+        if len(word) == 0:  # all the characters are checked
+            return True
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or word[0] != board[i][j]:
+            return False
+        tmp = board[i][j]  # first character is found, check the remaining part
+        board[i][j] = "#"  # avoid visit agian
+        # check whether can find "word" along one direction
+        res = self.dfs(board, i + 1, j, word[1:]) or self.dfs(board, i - 1, j, word[1:]) \
+        or self.dfs(board, i, j + 1, word[1:]) or self.dfs(board, i, j - 1, word[1:])
+        board[i][j] = tmp
+        return res

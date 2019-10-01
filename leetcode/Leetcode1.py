c13460a5af27c55d12bd86a52296e4bea3be673d
kami <1402702693@qq.com>
2019-10-01 11:37:10 +0800

    leetcode79题，去除重复字符多于2个，重排列集合。2个方法实现

--- leetcode/Leetcode1.py
+++ leetcode/Leetcode1.py
@@ -32,7 +32,6 @@ class Solution:
     The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or
     vertically neighboring. The same letter cell may not be used more than once.
     '''
-
     def exist(self, board, word):
         if not board:
             return False
@@ -56,3 +55,23 @@ class Solution:
         or self.dfs(board, i, j + 1, word[1:]) or self.dfs(board, i, j - 1, word[1:])
         board[i][j] = tmp
         return res
+
+    # 80 去除重复字符超过两个的字符，保留两个重复字符，返回字符长度
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # 双指针思想，跳过多余重复字符，重排列数组
+        if len(nums) < 3:
+            return len(nums)
+        pos = 1
+        for i in range(1, len(nums)-1):
+            if nums[i-1] != nums[i+1]:
+                nums[pos] = nums[i]
+                pos += 1
+        nums[pos] = nums[-1]
+        return pos+1
+
+    def removeDuplicates2(self, nums:List[int]) -> int:
+        # 统计每个字符的数量，同一字符多于2个的去除
+        for val in set(nums):
+            while nums.count(val) > 2:
+                nums.remove(val)
+        return len(nums)
